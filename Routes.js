cee5a54b02ea2ddfe97dc6060c66bc1851836358
Yannis Künzler <y.kuenzler@icloud.com>
2023-01-30 16:21:08 +0100

    added cors to supplements

--- Routes.js
+++ Routes.js
@@ -7,16 +7,8 @@ const {getFiliales} = require('./controllers/ShopController');
 const {healthCheck} = require('./controllers/APIController');
 
 router.get('/burgers', cors(), getBurgers);
-router.get('/supplements', getSupplements);
+router.get('/supplements', cors(), getSupplements);
 router.get('/filiales', getFiliales);
 router.get('/healthcheck', healthCheck);
-
-// router.options('*', (req, res) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', "DELETE");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//
-//     return res.status(200).end();
-// });
 
 module.exports = router;
